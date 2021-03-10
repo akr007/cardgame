9312fbfb72cd89a68dffc05b35614be5cb127189
akr007 <antalkrisztian0@gmail.com>
2021-03-10 10:44:34 +0100

    Basic start page and routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameComponent } from './game/game.component';
+import { StartComponent } from './start/start.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/start', pathMatch: 'full' },
+  { path: 'start', component: StartComponent },
+  { path: 'game', component: GameComponent }
 ];
 
 @NgModule({
--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-start',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+  }
+
+  onGame() {
+    
   }
 
 }
